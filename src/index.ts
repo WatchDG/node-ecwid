959c857e3042381e47480a829cc3f635d40f8d16
WatchDG <baranov@w3m.co>
2020-10-03 23:17:43 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import Axios, { AxiosInstance } from 'axios';
+import { ResultFail, ResultOk } from 'node-result';
+
+type StoreId = number;
+type StoreAccessToken = string;
+
+// Order
+type OrderId = number;
+
+class Ecwid {
+  private readonly storeId: StoreId;
+  private readonly storeAccessToken: StoreAccessToken;
+  private readonly instance: AxiosInstance;
+  constructor(storeId: StoreId, storeAccessToken: StoreAccessToken, timeout: number = 1000, headers: object = {}) {
+    this.storeId = storeId;
+    this.storeAccessToken = storeAccessToken;
+    const baseURL = ` https://app.ecwid.com/api/v3/${this.storeId}`;
+    const params = {
+      token: this.storeAccessToken,
+    };
+    this.instance = Axios.create({ baseURL, timeout, headers, params });
+  }
+
+  async getOrder(id: OrderId) {
+    try {
+      const { data } = await this.instance.get(`/orders/${id}`);
+      return ResultOk(data);
+    } catch (error) {
+      return ResultFail(error);
+    }
+  }
+}
